40500113ca0009f15f9177ff46bba99c70dd8fc8
jon-elofson <jonathan.elofson@gmail.com>
2015-06-19 21:31:48 -0700

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
